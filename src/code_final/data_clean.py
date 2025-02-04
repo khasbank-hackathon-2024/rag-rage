c3e540b0b636bc3196773996bb45f6a34785ca80
taivn <133628174+taivnaaiwnl@users.noreply.github.com>
2025-02-04 22:13:13 +0800

    Delete src/code_final directory
    rmv

--- src/code_final/data_clean.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd 
-import numpy as np 
-import joblib 
-
-
-df = pd.read_csv('data_dummy/khas_bank_branches.csv')
-df = df.drop(['type'], axis=1)
-s = f"{df.iloc[1]['name']} нь {df.iloc[1]['open_time']} нээж {df.iloc[1]['close_time']} хаадаг. Салбарын байршил{df.iloc[1]['address']}.Холбогдох утасны дугаар {df.iloc[1]['phone']}"
-print(s)
